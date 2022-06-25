fb3a1b9f86e42f10302eb7b9f9f04f40f3ea834e
mrmohsensami <mrmohsensami@gmail.com>
2022-06-25 16:28:42 +0430

    Single component added and get url api by fetch

---
+++ front/src/components/pages/Single.vue
@@ -0,0 +1,60 @@
+<template>
+
+<section class="bg-gray-100 py-8">
+<div class="container">
+    <div class="flex flex-col sm:flex-row gap-4">
+    <div class="sm:flex-1"><img :src="post._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url"></div>
+    <div class="flex flex-col justify-center items-center sm:flex-1">
+    <h1>{{ post.title.rendered }}</h1>
+    <div v-html="post.content.rendered"></div>
+    </div>
+</div>
+</div>
+</section>
+
+
+
+
+</template>
+
+<script>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
+export default {
+  setup() {
+    const post = ref({});
+    const loading = ref(true);
+    const route = useRoute();
+
+
+
+
+    function getPost() {
+        fetch(`https://webgaran.ir/wp-json/wp/v2/posts/${route.params.id}?_embed`)
+        .then(async (response) => {
+          const data = await response.json();
+          // check for error response
+          if (!response.ok) {
+            // get error message from body or default to response statusText
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+          }
+          post.value = data;
+        })
+        .catch((error) => {
+          this.errorMessage = error;
+          console.error("There was an error!", error);
+        });
+
+    }
+
+    getPost();
+
+    return { post, loading, route };
+  },
+};
+</script>
+
+<style>
+</style>
--- front/src/router.js
+++ front/src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './components/pages/Home.vue'
 import About from './components/pages/About.vue'
 import Contact from './components/pages/Contact.vue'
+import Single from './components/pages/Single.vue'
 
 const routes = [
     { path: '/', component: Home },
     { path: '/about', component: About },
     { path: '/contact', component: Contact },
+    { path: '/posts/:id', name: 'postId', component: Single }
 ]
 
 const router = createRouter({
