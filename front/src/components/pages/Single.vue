8b85858d88d13696b0065806b7e6ee708705fb0c
mohsensami <mohsensami88@gmail.com>
2022-06-28 00:20:12 +0430

    add comment api and show in single page

--- front/src/components/pages/Single.vue
+++ front/src/components/pages/Single.vue
@@ -20,6 +20,18 @@
       <div class="py-6" v-html="post.content.rendered"></div>
     </div>
   </section>
+
+  <section class="bg-gray-100 py-8">
+    <div class="container">
+      <div>
+        <h4 class="w-36 text-2xl mb-4 pb-2 border-gray-200 border-b-4 ">Comments</h4>
+        <ul>
+          <li v-html="comment.content.rendered" v-for="comment in comments" :key="comment.id">
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 </div>
 
 
@@ -32,11 +44,9 @@ import { useRoute } from "vue-router";
 export default {
   setup() {
     const post = ref({});
+    const comments = ref({});
     const loading = ref(true);
     const route = useRoute();
-
-
-
 
     function getPost() {
         fetch(`https://webgaran.ir/wp-json/wp/v2/posts/${route.params.id}?_embed`)
@@ -57,9 +67,30 @@ export default {
 
     }
 
+    function getComments() {
+        fetch(`https://webgaran.ir/wp-json/wp/v2/comments?post=${route.params.id}`)
+        .then(async (response) => {
+          const data = await response.json();
+          // check for error response
+          if (!response.ok) {
+            // get error message from body or default to response statusText
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+          }
+          comments.value = data;
+        })
+        .catch((error) => {
+          this.errorMessage = error;
+          console.error("There was an error!", error);
+        });
+
+    }
+
+    getComments();
+
     getPost();
 
-    return { post, loading, route };
+    return { post, comments, loading, route };
   },
 };
 </script>
